e3201e545353b6bf5d3910e40d860b6de2178c4a
student25 <pjfw78@gmail.com>
2022-04-03 15:23:25 +0200

    first commit

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,8 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
+import org.hibernate.engine.transaction.internal.TransactionImpl;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -22,4 +24,30 @@ public class ParticipantService {
 		return query.list();
 	}
 
+	//jesli jest szukanie tylko po id to jest ponizszy kod, nie trzeba robic hql
+	public Participant findByLogin(String login){
+//		String hql = "FROM Participant WHERE Participant.login ="+login;
+//		Query query = connector.getSession().createQuery(hql);
+//		return query.;
+		return  (Participant)  connector.getSession().get(Participant.class, login);
+	}
+
+	public void delete(Participant participant){
+		Transaction transaction= connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+	}
+	public void add(Participant participant){
+		Transaction transaction= connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+
+	public void update(Participant participant) {
+		Transaction transaction= connector.getSession().beginTransaction();
+		connector.getSession().update(participant);
+		transaction.commit();
+	}
 }
+
+

1d3e8ab8ece8a94d86da56db32b1c2ecc64017f9
Regis <pjfw78@gmail.com>
2022-05-01 23:56:18 +0200

    Premium resolution

---
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -0,0 +1,153 @@
+package com.company.enroller.controllers;
+
+
+import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
+import com.company.enroller.persistence.MeetingService;
+import com.company.enroller.persistence.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+@RestController
+@RequestMapping("/meetings")
+public class MeetingRestController {
+
+    @Autowired
+    MeetingService meetingService;
+
+    @Autowired
+    ParticipantService participantService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetings() {
+        Collection<Meeting> meetings = meetingService.getAll();
+        return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "sortByTitle/{orderBy}", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetingsSortBy(@PathVariable("orderBy") String orderBy) {
+        Collection<Meeting> meetings = meetingService.getAllSortedByTitle(orderBy);
+        return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "contains/{word}", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetingsContains(@PathVariable("word") String word) {
+        Collection<Meeting> meetings = meetingService.getAllContains(word);
+        return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "containsUser/{login}", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetingsContainsUser(@PathVariable("login") String login) {
+        Participant participant = participantService.findByLogin(login);
+        if (participant == null) {
+            return new ResponseEntity("Not Found participant", HttpStatus.NOT_FOUND);
+        }
+        Collection<Meeting> meetings = meetingService.getAll();
+
+        ArrayList<Meeting> foundMeetings = new ArrayList<>();
+        for (Meeting meeting : meetings) {
+            Collection<Participant> participants = meeting.getParticipants();
+            if (participants.contains(participant)) {
+                foundMeetings.add(meeting);
+            }
+        }
+        return new ResponseEntity<Collection<Meeting>>(foundMeetings, HttpStatus.OK);
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeeting(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity("Not Found",HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteMeeting(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity("Not Found",HttpStatus.NOT_FOUND);
+        }
+        meetingService.delete(meeting);
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<?> addMeeting(@RequestBody Meeting meeting){
+        Meeting foundMeeting = meetingService.findById(meeting.getId());
+        if (foundMeeting != null) {
+            return new ResponseEntity("Unable to create. A meeting with id " + meeting.getId() + " already exist.", HttpStatus.CONFLICT);
+        }
+        meetingService.add(meeting);
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT	)
+    public ResponseEntity<?> updateMeeting(@PathVariable("id") long id, @RequestBody Meeting meeting) {
+        Meeting foundMeeting = meetingService.findById(meeting.getId());
+        if (foundMeeting == null) {
+            return new ResponseEntity("Not Found",HttpStatus.NOT_FOUND);
+        }
+        foundMeeting.setTitle(meeting.getTitle());
+        foundMeeting.setDescription(meeting.getDescription());
+        foundMeeting.setDate(meeting.getDate());
+        meetingService.update(foundMeeting);
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/{id}/participants", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetingParticipants(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity("Not Found",HttpStatus.NOT_FOUND);
+        }
+//        meeting = meetingService.getMeetingParticipants(id);
+        return new ResponseEntity<Collection<Participant>>(meeting.getParticipants(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}/participant/{id2}", method = RequestMethod.POST)
+    public ResponseEntity<?> addMeetingParticipant(@PathVariable("id") long id,@PathVariable("id2") String login) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity("Not Found meeting",HttpStatus.NOT_FOUND);
+        }
+        Participant participant = participantService.findByLogin(login);
+        if (participant == null) {
+            return new ResponseEntity("Not Found participant",HttpStatus.NOT_FOUND);
+        }
+        Collection<Participant> participants = meeting.getParticipants();
+        if (participants.contains(participant)){
+            return new ResponseEntity("Participant already added",HttpStatus.ALREADY_REPORTED);
+        }
+        meeting.addParticipant(participant);
+        meetingService.update(meeting);
+        return new ResponseEntity<Collection<Participant>>(meeting.getParticipants(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}/participant/{id2}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteMeetingParticipant(@PathVariable("id") long id,@PathVariable("id2") String login) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity("Not Found meeting",HttpStatus.NOT_FOUND);
+        }
+        Participant participant = participantService.findByLogin(login);
+        if (participant == null) {
+            return new ResponseEntity("Not Found participant",HttpStatus.NOT_FOUND);
+        }
+        Collection<Participant> participants = meeting.getParticipants();
+        if (!participants.contains(participant)){
+            return new ResponseEntity("Not Found participant attached to meeting",HttpStatus.NOT_FOUND);
+        }
+        meeting.removeParticipant(participant);
+        meetingService.delete(meeting);
+        return new ResponseEntity<Collection<Participant>>(meeting.getParticipants(), HttpStatus.OK);
+    }
+}
--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,11 +1,9 @@
 package com.company.enroller.persistence;
 
 import java.util.Collection;
-
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
-
 import com.company.enroller.model.Meeting;
 
 @Component("meetingService")
@@ -23,4 +21,48 @@ public class MeetingService {
 		return query.list();
 	}
 
+	public Collection<Meeting> getAllSortedByTitle(String order) {
+		String hql="";
+		if (order.equals("ASC")){
+			hql = "FROM Meeting m ORDER BY m.title ASC";
+		} else {
+			hql = "FROM Meeting m ORDER BY m.title DESC";
+		}
+		Query query = connector.getSession().createQuery(hql);
+		return query.list();
+	}
+
+	public Collection<Meeting> getAllContains(String word) {
+		String hql = "FROM Meeting m where m.title like :word or m.description like :word";
+		Query query = connector.getSession().createQuery(hql);
+		query.setParameter("word","%"+word+"%");
+		return query.list();
+	}
+
+	public Meeting findById(long id){
+		return  (Meeting)  connector.getSession().get(Meeting.class, id);
+	}
+	public void delete(Meeting meeting){
+		Transaction transaction= connector.getSession().beginTransaction();
+		connector.getSession().delete(meeting);
+		transaction.commit();
+	}
+	public void add(Meeting meeting){
+		Transaction transaction= connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction= connector.getSession().beginTransaction();
+		connector.getSession().update(meeting);
+		transaction.commit();
+	}
+
+//	public Meeting getMeetingParticipants(long id) {
+//		String hql = "FROM Meeting M WHERE M.id = :meeting_id ";
+//		Query query = connector.getSession().createQuery(hql);
+//		query.setParameter("meeting_id",id);
+//		return query.;
+//	}
 }
